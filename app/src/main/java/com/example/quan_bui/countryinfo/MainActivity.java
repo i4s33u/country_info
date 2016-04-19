701fd9d45e05d1e682cc040eda1a4fe131c4dc18
quan.bui <quan.bui@jmango360.com>
2016-04-19 09:43:01 +0700

    implement matchData method

--- app/src/main/java/com/example/quan_bui/countryinfo/MainActivity.java
+++ app/src/main/java/com/example/quan_bui/countryinfo/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import com.example.quan_bui.countryinfo.adapter.PersonAdapter;
@@ -15,7 +16,7 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
+import rx.Subscriber;
 import rx.schedulers.Schedulers;
 
 public class MainActivity
@@ -76,19 +77,51 @@ public class MainActivity
             @Override
             public void onClick(View v) {
 
-                service.getCountries()
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .flatMap(Observable::from)
-                    .zipWith(Observable.from(personList),
-                             (country, person) -> combine(country, person))
-                    .subscribe(person -> adapter.add(person));
+                Observable.from(personList)
+                    .zipWith(service.getCountries(),
+                             (person, countries) -> matchData(person, countries))
+                    .subscribe(new Subscriber<Person>() {
+                        @Override
+                        public void onCompleted() {
+
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+
+                        }
+
+                        @Override
+                        public void onNext(Person person) {
+                            adapter.add(person);
+                        }
+                    });
+
+                //service.getCountries()
+                //    .subscribeOn(Schedulers.newThread())
+                //    .observeOn(AndroidSchedulers.mainThread())
+                //    .flatMap(Observable::from)
+                //    .zipWith(Observable.from(personList),
+                //             (country, person) -> matchData(country, person))
+                //    .subscribe(person -> adapter.add(person));
             }
         });
     }
 
-    public Person combine(Country country, Person person) {
-        person.setCountryName(country.getName());
+    //public Person combine(Country country, Person person) {
+    //    person.setCountryName(country.getName());
+    //    return person;
+    //}
+
+    public Person matchData(Person person, List<Country> countries) {
+
+        for (Country country : countries) {
+            if (country.getCountryCode().equalsIgnoreCase(person.getCountryCode())) {
+                person.setCountryName(country.getName());
+            }
+            Log.d("Match or not ? ",
+                  "" + country.getCountryCode().equalsIgnoreCase(person.getCountryCode()));
+        }
         return person;
     }
 }
